6bd8bfe982f716a5ffb618e46caf0679740946a0
Do Trung Kien <trungkien.keio@gmail.com>
2019-11-29 08:08:51 +0700

    refactor

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-import { AppConfig } from 'blockstack'
+import { AppConfig } from 'blockstack';
 
-export const appConfig = new AppConfig(['store_write', 'publish_data'])
+export const appConfig = new AppConfig(['store_write', 'publish_data']);
 export const ANIMALS = [
   {
     id: 'cat',
@@ -16,7 +16,8 @@ export const ANIMALS = [
     id: 'lion',
     name: 'Lion',
     superpower: 'Roaring loudly.'
-  }]
+  }
+];
 
 export const TERRITORIES = [
   {
@@ -28,13 +29,17 @@ export const TERRITORIES = [
     id: 'tundra',
     name: 'Tundra',
     superpower: 'Let it snow!'
+  },
+  {
+    id: 'westeros',
+    name: 'Westeros',
+    superpower: 'The Iron Throne!'
   }
-]
+];
 
-export const ME_FILENAME = 'me.json'
-export const SUBJECTS_FILENAME = 'subjects.json'
-export const EXPLORER_URL = 'https://explorer.blockstack.org'
-
+export const ME_FILENAME = 'me.json';
+export const SUBJECTS_FILENAME = 'subjects.json';
+export const EXPLORER_URL = 'https://explorer.blockstack.org';
 
 export const OTHER_KINGDOMS = [
   {
@@ -50,7 +55,11 @@ export const OTHER_KINGDOMS = [
     ruler: 'yannael_leborgne.id'
   },
   {
-      app: 'https://thirsty-jang-0c0a17.netlify.com',
-      ruler: 'ma1222042.id.blockstack'
+    app: 'https://thirsty-jang-0c0a17.netlify.com',
+    ruler: 'ma1222042.id.blockstack'
+  },
+  {
+    app: 'https://animalkingdoms.netlify.com',
+    ruler: 'meepers.id.blockstack'
   }
-]
+];
